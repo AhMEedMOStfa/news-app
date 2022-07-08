f3957afa3ad550ce4d05763c04a035cf5380ec3c
Ziad Hosny <ziadhosny007#gmail.com>
2022-07-08 14:47:07 +0200

    update login page

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,51 +2,51 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LocalStorageService } from 'src/app/Services/local-storage.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  notLoggedIn: boolean = false;
+  notCorrect: boolean = false;
   state: string = '';
 
   constructor(
-    private regForm: FormBuilder,
+    private _fb: FormBuilder,
     private _localstorageService: LocalStorageService,
-    private router: Router
+    private _router: Router
   ) {}
 
-  loginForm: FormGroup = this.regForm.group({
+  loginForm: FormGroup = this._fb.group({
     email: [
-      null,
+      '',
       [
         Validators.required,
         Validators.email,
         Validators.pattern(/^[^\s@]+@[^\s@]+\.[^\s@]+$/),
       ],
     ],
-    password: [null, [Validators.required]],
+    password: ['', [Validators.required]],
   });
 
   submitForm() {
     if (this.loginForm.valid) {
-      let userName = this._localstorageService.getLocalStorage(
+      let correct = this._localstorageService.getLocalStorage(
         this.loginForm.value.email,
         this.loginForm.value.password
       );
 
-      if (userName) {
-        console.log(userName);
-        this.router.navigate(['/home']);
+      if (correct) {
+        this._router.navigate(['/home']);
       } else {
-        this.notLoggedIn = true;
+        this.notCorrect = true;
       }
     }
   }
 
   checkAuth() {
-      return localStorage.setItem('auth', 'true');
+    return localStorage.setItem('auth', 'true');
   }
 
   onChange(e: any) {
